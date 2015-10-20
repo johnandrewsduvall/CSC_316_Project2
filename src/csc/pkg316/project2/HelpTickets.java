12b46833ce7034908781ab81ea864afc8cbb635a
John Andrew S Duvall <johnandew.duvall@gmail.com>
2015-10-20 10:51:28 -0400

    Initial Commit

---
+++ src/csc/pkg316/project2/HelpTickets.java
@@ -0,0 +1,17 @@
+/*
+ * Author:     John Andrew S Duvall
+ * Date:       date
+ * Project:    project
+ */
+package csc.pkg316.project2;
+
+public class HelpTickets {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
